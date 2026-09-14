c3024bd5d4a17d83a78cf253063945675549c38e
kalethgarcia76 <kalethgarcia76@gmail.com>
2026-09-14 04:28:10 +0000

    taller3: creacion de init

---
+++ logica_difusa/__init__.py
@@ -0,0 +1,12 @@
+from .membresia import membresia_triangular, ConjuntoDifuso
+from .base_conjuntos_experiencia import CONJUNTOS_EXPERIENCIA
+from .clasificador import evaluar_experiencia, evaluar_conductores, ResultadoFuzzificacion
+
+__all__ = [
+    "membresia_triangular",
+    "ConjuntoDifuso",
+    "CONJUNTOS_EXPERIENCIA",
+    "evaluar_experiencia",
+    "evaluar_conductores",
+    "ResultadoFuzzificacion",
+]
