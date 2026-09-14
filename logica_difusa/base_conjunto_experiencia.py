4dfea5aff3002883224870002bc7c7dfb707bce6
kalethgarcia76 <kalethgarcia76@gmail.com>
2026-09-14 04:27:27 +0000

    taller3: creacion de base conjunto

---
+++ logica_difusa/base_conjunto_experiencia.py
@@ -0,0 +1,8 @@
+
+from .membresia import ConjuntoDifuso
+
+CONJUNTOS_EXPERIENCIA = [
+    ConjuntoDifuso(nombre="Novato", a=0, b=0, c=5),
+    ConjuntoDifuso(nombre="Intermedio", a=2, b=5, c=8),
+    ConjuntoDifuso(nombre="Experto", a=5, b=10, c=20),
+]
